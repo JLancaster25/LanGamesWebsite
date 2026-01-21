920ba90361125b005d73c72fe181a5b9cdd9e9cf
JLancaster25 <austin.lancaster7@gmail.com>
2026-01-20 19:21:15 -0600

    Add files via upload

---
+++ website/bingo/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  'https://kppgmvfdfuhmtuaukkdn.supabase.co',
+  'sb_publishable_e4AhlY9ZIgdlsG8rl111Fg_tWghrBW4'
+);
