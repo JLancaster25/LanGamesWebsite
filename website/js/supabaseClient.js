a4438e65cfd93a3262254b4c7f7c31c626bd10a3
JLancaster25 <austin.lancaster7@gmail.com>
2026-01-23 14:27:56 -0600

    Add files via upload

---
+++ website/js/supabaseClient.js
@@ -0,0 +1,9 @@
+// js/supabaseClient.js
+
+const SUPABASE_URL = "https://kppgmvfdfuhmtuaukkdn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_e4AhlY9ZIgdlsG8rl111Fg_tWghrBW4";
+
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
