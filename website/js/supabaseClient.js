73edf704c0c4bcc20fb975d27355d3032a2a170b
JLancaster25 <austin.lancaster7@gmail.com>
2026-01-30 02:45:39 -0600

    Update supabaseClient.js

--- website/js/supabaseClient.js
+++ website/js/supabaseClient.js
@@ -1,6 +1,5 @@
 // IMPORTANT:
 // This file is loaded ONCE and owns Supabase completely.
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 const SUPABASE_URL = "https://kppgmvfdfuhmtuaukkdn.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_e4AhlY9ZIgdlsG8rl111Fg_tWghrBW4";
 
@@ -12,4 +11,5 @@ const supabaseClient = window.supabase.createClient(
 
 // Expose a SINGLE global reference
 window.supabaseClient = supabaseClient
+
 
