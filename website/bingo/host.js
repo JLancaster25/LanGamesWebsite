65a97d217e982372de2bafcaafef52145bb8470e
JLancaster25 <austin.lancaster7@gmail.com>
2026-02-02 04:49:50 -0600

    Update host.js

--- website/bingo/host.js
+++ website/bingo/host.js
@@ -72,7 +72,7 @@ if (!user) {
   `;
 
   document.getElementById("loginBtn").onclick = () => {
-    window.location.href = "/websiteLogin/login.html";
+    window.location.href = "/WebsiteLogin/login.html";
   };
 
   return;
@@ -263,6 +263,7 @@ async function endGame() {
   await sb.from("games").update({ status: "finished" }).eq("id", gameId);
   speak("Game over");
 }
+
 
 
 
